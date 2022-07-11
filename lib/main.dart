8f7a03dea4da0dc6474eadd441b2221bc47a96a5
Guilherme Duarte Giesbrecht <guidgiesbrecht@gmail.com>
2022-07-11 18:09:53 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ByteBankApp());
+}
+
+class ByteBankApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Image.asset('images/bytebank_logo.png'),
+          ],
+        ),
+      ),
+    );
+  }
+}
